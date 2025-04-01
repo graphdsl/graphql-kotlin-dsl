ad7a4e7d0f2f099775733b2c2ebc3e96d7cbd13e
Kristi Leka <m.kristileka@gmail.com>
2025-04-01 11:22:00 +0200

    Add common settings convention

---
+++ build-logic/src/main/kotlin/settings/common.settings.gradle.kts
@@ -0,0 +1,23 @@
+package settings
+
+pluginManagement {
+    plugins {
+        id("org.jetbrains.dokka")
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention")
+}
+
+@Suppress("UnstableApiUsage")
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+    repositoriesMode = RepositoriesMode.FAIL_ON_PROJECT_REPOS
+}
+
+gradle.allprojects {
+    group = "io.github.graphdsl"
+}
