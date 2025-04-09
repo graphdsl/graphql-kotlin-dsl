e3874cd1ef03393d9bc8266768e4e98b77bd1ca0
Kristi Leka <m.kristileka@gmail.com>
2025-04-09 10:15:00 +0200

    Add Kotlin convention for library-only modules

---
+++ build-logic/src/main/kotlin/conventions/kotlin-without-tests.gradle.kts
@@ -0,0 +1,21 @@
+package conventions
+
+import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    idea
+    id("org.jetbrains.kotlin.jvm")
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+tasks.withType<KotlinCompile>().configureEach {
+    compilerOptions {
+        apiVersion = KotlinVersion.KOTLIN_1_8
+        languageVersion = KotlinVersion.KOTLIN_1_8
+    }
+}
