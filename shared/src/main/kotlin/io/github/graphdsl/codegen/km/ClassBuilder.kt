61b674ee9af1b7f69a1999dc27676ff9b6a284ac
Kristi Leka <m.kristileka@gmail.com>
2025-08-04 09:28:00 +0200

    Add shared: class builder interface

---
+++ shared/src/main/kotlin/io/github/graphdsl/codegen/km/ClassBuilder.kt
@@ -0,0 +1,10 @@
+package io.github.graphdsl.codegen.km
+
+import io.github.graphdsl.codegen.ct.KmClassTree
+
+/** The external API to this package returns these ClassBuilders to
+ *  clients.  Subclasses allow those clients to populate a ClassBuilder
+ *  but not to actually build one themselves - that's done internally. */
+abstract class ClassBuilder {
+    internal abstract fun build(): KmClassTree
+}
