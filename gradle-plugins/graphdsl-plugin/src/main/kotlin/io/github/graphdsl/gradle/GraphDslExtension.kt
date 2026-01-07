dc6e80c6583f6a4bfc3b724c24d14c666d7043c5
Kristi Leka <m.kristileka@gmail.com>
2026-01-07 10:45:00 +0100

    Add graphdsl-plugin: extension DSL configuration

---
+++ gradle-plugins/graphdsl-plugin/src/main/kotlin/io/github/graphdsl/gradle/GraphDslExtension.kt
@@ -0,0 +1,30 @@
+package io.github.graphdsl.gradle
+
+import org.gradle.api.model.ObjectFactory
+import org.gradle.api.provider.Property
+
+/**
+ * Extension for configuring the GraphDSL code generation plugin.
+ *
+ * Usage in build.gradle.kts:
+ * ```kotlin
+ * graphDsl {
+ *     packageName.set("com.example.api.dsl")
+ * }
+ * ```
+ */
+open class GraphDslExtension(objects: ObjectFactory) {
+    /**
+     * The Kotlin package name for generated DSL classes.
+     * Defaults to "io.github.graphdsl.dsl".
+     */
+    val packageName: Property<String> = objects.property(String::class.java)
+        .convention("io.github.graphdsl.dsl")
+
+    /**
+     * The directory containing GraphQL schema files (.graphqls).
+     * Defaults to "src/main/graphql".
+     */
+    val schemaDir: Property<String> = objects.property(String::class.java)
+        .convention("src/main/graphql")
+}
