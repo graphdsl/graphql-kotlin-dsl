980895bc0ab53f49e4b443840954b1dd3bd3f3b6
Kristi Leka <m.kristileka@gmail.com>
2026-02-24 14:25:40 +0100

    Fixes build for release

--- gradle-plugins/settings.gradle.kts
+++ gradle-plugins/settings.gradle.kts
@@ -25,3 +25,16 @@ plugins {
 
 includeNamed(":common")
 includeNamed(":graphdsl-plugin")
+
+// Substitute core library modules from source so this build can be run standalone
+// (e.g. via --project-dir gradle-plugins in CI) without requiring published artifacts.
+includeBuild("../included-builds/core") {
+    dependencySubstitution {
+        substitute(module("io.github.graphdsl:cli")).using(project(":cli"))
+        substitute(module("io.github.graphdsl:codegen")).using(project(":codegen"))
+        substitute(module("io.github.graphdsl:mapper")).using(project(":mapper"))
+        substitute(module("io.github.graphdsl:schema")).using(project(":schema"))
+        substitute(module("io.github.graphdsl:shared")).using(project(":shared"))
+        substitute(module("io.github.graphdsl:utils")).using(project(":utils"))
+    }
+}
